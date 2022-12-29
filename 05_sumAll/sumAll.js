b1b24844b7d92f828ac6e25839e88787d99e88ed
Anthony <acr1191@gmail.com>
2022-12-29 15:12:05 -0800

    add solution for 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let sum = 0;
+    if (a < 0 || b < 0 || typeof(a) !== "number" || typeof(b) !== "number") {
+        return "ERROR";
+    } else {
+        let min = a;
+        let max = b;
+        if (a > b) {
+            min = b;
+            max = a;
+        }
+        for (let i = min; i <= max; i++){
+            sum += i;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
