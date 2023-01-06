e02419af76d36eafa1d75cfd5f2e9c787a63754a
Anthony <acr1191@gmail.com>
2023-01-05 18:41:10 -0800

    add solution for 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (a) {
+    const originalString = String(a).split('').filter(char => /[a-zA-Z]/.test(char));
+    const original = originalString.join().toLowerCase();
+    const reverse = originalString.reverse().join().toLowerCase();
+    return original === reverse;
 };
 
 // Do not edit below this line
