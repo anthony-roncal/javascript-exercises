f8cdd194b5af78839fc3d6a009aef1e25ca4901c
Anthony <acr1191@gmail.com>
2023-01-05 19:01:14 -0800

    add solution for 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    let fibArray = [1, 1];
+    if (index < 1) {
+        return "OOPS";
+    } else {
+        while (fibArray.length <= index) {
+            fibArray.push(fibArray[fibArray.length - 1] + fibArray[fibArray.length - 2]);
+        }
+        return fibArray[index-1];
+    }
+    
 };
 
 // Do not edit below this line
