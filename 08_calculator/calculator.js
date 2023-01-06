7cb5d4a09cefa011e311854039386ca9e0d19e97
Anthony <acr1191@gmail.com>
2023-01-05 18:20:38 -0800

    add solution for 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
 const add = function() {
-	
+  const initial = 0;
+	const sum = Array.from(arguments).reduce((sum, number) => sum + number, initial);
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;	
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	const initial = 0;
+  const sum = array.reduce((acc, number) => acc + number, initial);
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  const initial = 1;
+  const product = array.reduce((acc, number) => acc * number, initial);
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  return (a > 1) ? a * factorial(a-1) : 1;
 };
 
 // Do not edit below this line
